de700a69411d85fd4f316bf96d11dd76e1ed1d62
sunny <sunny@cleaninglab.co.kr>
2019-08-20 09:02:28 +0900

    Initial Commit

---
+++ DataStructure_C/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DataStructure_C
+//
+//  Created by Seonghun Kim on 20/08/2019.
+//  Copyright © 2019 Seonghun Kim. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
